2bf6246ea1b52f51b491acf0437f9424222e2456
Inooid <inooid@gmail.com>
2022-08-08 12:28:25 +0200

    Update minimum elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SlackAPIDocs.MixProject do
     [
       app: :slack_api_docs,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: ">= 1.10.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
