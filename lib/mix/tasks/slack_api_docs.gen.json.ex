cc0b9fe1e42da1ddfb0d17b03c8f44936b2d894a
Inooid <inooid@gmail.com>
2022-08-08 12:44:27 +0200

    Add IO.inspect to gather insights into args

--- lib/mix/tasks/slack_api_docs.gen.json.ex
+++ lib/mix/tasks/slack_api_docs.gen.json.ex
@@ -30,6 +30,8 @@ defmodule Mix.Tasks.SlackApiDocs.Gen.Json do
     output_path = List.first(args) || @default_output_path
     concurrency = opts[:concurrency]
 
+    IO.inspect(args)
+
     original_shell = Mix.shell()
     if opts[:quiet], do: Mix.shell(Mix.Shell.Quiet)
 
